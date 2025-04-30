47e1ca4a03794d64917005701584f3232660c322
Giovanni <giovannimaldonado07@gmail.com>
2025-04-29 23:57:59 -0400

    put the query calls in a try and catch and send message with teh correct error

--- Backend/db.ts
+++ Backend/db.ts
@@ -14,14 +14,37 @@ interface User {
 	Id: number;
 	Name: string;
 	LastName: string;
-	Code: string;
+	Code: number;
 }
 
-export async function checkCode(code: string): Promise<User[]> {
-	const [results] = await db.query("SELECT * FROM user WHERE Code = ?", [
-		code,
-	]);
-	return results as User[];
+export async function checkCode(code: number): Promise<User[]> {
+	try {
+		const [results] = await db.query("SELECT * FROM user WHERE Code = ?", [
+			code,
+		]);
+		return results as User[];
+	} catch (error: any) {
+		console.error("Error checking code in database:", error);
+		throw new Error("Error checking code");
+	}
+}
+export async function addUser(
+	user: User
+): Promise<{ insertId: number; affectedRows: number }> {
+	const { Name, LastName, Code } = user;
+	try {
+		const [res]: any = await db.query(
+			"INSERT INTO user (Name, LastName, Code) VALUES (?, ?, ?)",
+			[Name, LastName, Code]
+		);
+		console.log("User added db:", user);
+		return { insertId: res.insertId, affectedRows: res.affectedRows };
+	} catch (error: any) {
+		if (error.code === "ER_DUP_ENTRY") {
+			throw new Error("Code already exists");
+		}
+		throw error;
+	}
 }
 
 export default db;
